6b1a77c1271151a41c18f56a9c3903d929d0581f
Eduarda Lopes <149630560+FreitasLopes@users.noreply.github.com>
2024-11-15 02:13:35 -0300

    atualizações_login_perfil
    -inserindo a coluna "nome" no db
    - mandando os dados para a Perfilactivity
    - retirando mensagens de erros tecnicos
    - ajustando servidor (o servidor esta zipado)

--- src/Frontend/AppAppoef/app/src/main/java/org/appoef/appappoef/PerfilActivity.java
+++ src/Frontend/AppAppoef/app/src/main/java/org/appoef/appappoef/PerfilActivity.java
@@ -1,20 +1,33 @@
 package org.appoef.appappoef;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 public class PerfilActivity extends AppCompatActivity {
+    TextView usuarioNome, usuarioEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_perfil);
+
+        usuarioNome = findViewById(R.id.textNomeCompleto);
+       usuarioEmail = findViewById(R.id.textEmail);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("login_prefs", MODE_PRIVATE);
+        String nome = sharedPreferences.getString("nome", "Usuário não encontrado");
+        String email = sharedPreferences.getString("email", "Email não encontrado");
+
+        usuarioNome.setText("Nome: " + nome);
+       usuarioEmail.setText("Email: " + email);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
