164a034aa3e884a7d9970d83ab3681000697f46f
Shoucchann <145676372+Shoucchann@users.noreply.github.com>
2024-11-18 17:41:36 -0300

    Configuracoes
    XML e Java para as configurações e botão de sair para o Perfil

--- src/Frontend/AppAppoef/app/src/main/java/org/appoef/appappoef/ConfiguracoesActivity.java
+++ src/Frontend/AppAppoef/app/src/main/java/org/appoef/appappoef/ConfiguracoesActivity.java
@@ -1,6 +1,11 @@
 package org.appoef.appappoef;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +14,41 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ConfiguracoesActivity extends AppCompatActivity {
+    TextView usuarioNome, usuarioEmail, usuarioTelefone, usuarioSenha;
+    private Button btnSair;
+    private TextView txtEsqueciSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_configuracoes);
+
+        usuarioNome = findViewById(R.id.textInputEditNome);
+        usuarioEmail = findViewById(R.id.textInputEditEmail);
+        usuarioTelefone = findViewById(R.id.textInputEditTelefone);
+        usuarioSenha = findViewById(R.id.textInputEditSenha);
+        btnSair = findViewById(R.id.btnSair);
+        txtEsqueciSenha = findViewById(R.id.txtEsqueciSenha);
+
+        btnSair.setOnClickListener(v -> {
+            Intent intent = new Intent(ConfiguracoesActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        });
+
+        SharedPreferences sharedPreferences = getSharedPreferences("login_prefs", MODE_PRIVATE);
+
+        String nome = sharedPreferences.getString("nome", "Usuário não encontrado");
+        String email = sharedPreferences.getString("email", "Email não encontrado");
+        String telefone = sharedPreferences.getString("telefone", "Telefone não encontrado");
+        String senha = sharedPreferences.getString("senha", "Senha não encontrada");
+
+        usuarioNome.setText(nome);
+        usuarioEmail.setText(email);
+        usuarioTelefone.setText(telefone);
+        usuarioSenha.setText(senha);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- src/Frontend/AppAppoef/app/src/main/java/org/appoef/appappoef/PerfilActivity.java
+++ src/Frontend/AppAppoef/app/src/main/java/org/appoef/appappoef/PerfilActivity.java
@@ -1,7 +1,9 @@
 package org.appoef.appappoef;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -11,6 +13,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 public class PerfilActivity extends AppCompatActivity {
     TextView usuarioNome, usuarioEmail;
+    private Button btnSair;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +21,21 @@ public class PerfilActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_perfil);
 
-        usuarioNome = findViewById(R.id.textNomeCompleto);
+       usuarioNome = findViewById(R.id.textNomeCompleto);
        usuarioEmail = findViewById(R.id.textEmail);
+       btnSair = findViewById(R.id.btnSair);
+
+        btnSair.setOnClickListener(v -> {
+            Intent intent = new Intent(PerfilActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
 
         SharedPreferences sharedPreferences = getSharedPreferences("login_prefs", MODE_PRIVATE);
         String nome = sharedPreferences.getString("nome", "Usuário não encontrado");
         String email = sharedPreferences.getString("email", "Email não encontrado");
 
         usuarioNome.setText("Nome: " + nome);
-       usuarioEmail.setText("Email: " + email);
+        usuarioEmail.setText("Email: " + email);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
